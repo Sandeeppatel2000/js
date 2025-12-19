224d231620816337eef9f26d42d481c00fe90a09
Sandeep Patel <49168110+Sandeeppatel2000@users.noreply.github.com>
2025-12-19 06:48:06 +0000

    variable and datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 23749
+let accountEmail = "test@gmail.com"
+var accountPassword = "223"
+accountCity = "HP"
+
+// accountId = 2  not allowed
+
+
+accountEmail = "ddd@gmail.com"
+
+let accountState
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert(2+2) // we are using nodejs, not browser
+
+let name = "John"
+
+let age = 18 
+
+let isLoggedIn = false
+
+// number ranget -> 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value 
+//undefined => no value is defined
+// symbol => unique 
+
+//object 
+console.log(typeof undefined);
+console.log(typeof null);
+
+
+
+
