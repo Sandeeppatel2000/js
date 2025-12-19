a83f83b7af267130cafbc60f61922c08c42f6243
Sandeep Patel <49168110+Sandeeppatel2000@users.noreply.github.com>
2025-12-19 07:26:51 +0000

    stacks and heap

--- 01_basics/data_types-summary.js
+++ 01_basics/data_types-summary.js
@@ -1,8 +1,10 @@
 // Primtive 
 // 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+// Stack memory
 
 // Non-primitive datatype (Reference)
 // Array, Objects, Functions
+// Heap
 
 // JavaScript is a dynamically typed programming language. This means that you do not need to explicitly declare the data type of a variable when you create it, and the type of a variable can change during runtime (when the program is executing). 
 
