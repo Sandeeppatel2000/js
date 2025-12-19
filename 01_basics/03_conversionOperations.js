7290d721f29620f658c660a1fba067dfd49882d5
Sandeep Patel <49168110+Sandeeppatel2000@users.noreply.github.com>
2025-12-19 07:08:42 +0000

    datatype conversions

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,63 @@
+let score = "true729472"
+
+// console.log(typeof score);
+// console.log(typeof(score))
+
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 / false=>0
+// undefined => NaN 
+// null => 0
+//"" => false 
+//"test" => true
+
+
+let num = 26283
+let strNum = String(num)
+
+// console.log(strNum)
+// console.log(typeof strNum);
+
+console.log("***********************************")
+
+// ***************Operation****************
+
+let value = 3 
+let negValue = -value 
+// console.log(negValue);
+
+
+
+let str1 = "hello"
+let str2 = " helll"
+
+let str3 = str1 + str2
+// console.log(str3);
+// console.log(1+"2");
+// console.log("1"+2)
+// console.log("1"+"2")
+// console.log("1"+1+2)
+// console.log(1+2+"1");
+
+// console.log(+true);
+// console.log(+"");
+
+
+let num1, num2, num3 
+
+num1=num2=num3 = 2+2
+
+let gameCounter = 100 
+gameCounter++
+console.log(gameCounter);
+
+
+
+
+
